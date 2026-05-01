093fa3ad5864b21f8a1d5ebf26a76c648c1ecd09
andrej-nemanic <andrej.nemanic@student.um.si>
2026-05-01 12:25:46 +0200

    SCRUM-16 Naredil sem app navigation

--- DesktopApplication/composeApp/src/jvmMain/kotlin/com/example/desktopapplication/main.kt
+++ DesktopApplication/composeApp/src/jvmMain/kotlin/com/example/desktopapplication/main.kt
@@ -1,16 +1,5 @@
 package com.example.desktopapplication
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
