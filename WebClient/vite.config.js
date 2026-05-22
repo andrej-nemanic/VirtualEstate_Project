85b6dac952d0252a6927b31150e30927ca06c0b2
NikSignjarZilavec <nik.signjar@student.um.si>
2026-05-22 17:34:32 +0200

    SCRUM-61 fixed dockerfile connection

--- WebClient/vite.config.js
+++ WebClient/vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    host: '0.0.0.0', 
+    proxy: {
+      '/api': {
+        target: 'http://web-service:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 });
